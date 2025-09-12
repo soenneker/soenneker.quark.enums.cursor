6fdcfd93e504205defaa70f429b0d30b8e3f6608
Jake Soenneker <jake@soenneker.com>
2025-09-12 06:21:08 -0500

    Initial commit

---
+++ test/Soenneker.Quark.Enums.Cursor.Tests/CursorTests.cs
@@ -0,0 +1,13 @@
+﻿using Soenneker.Tests.Unit;
+using Xunit;
+
+namespace Soenneker.Quark.Enums.Cursor.Tests;
+
+public sealed class CursorTests : UnitTest
+{
+    [Fact]
+    public void Default()
+    {
+
+    }
+}
